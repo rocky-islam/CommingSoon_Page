8a0645668c6a2154cda5b01eed8aeded9e14203a
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2023-05-03 15:23:37 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Stylesheets
+import './index.css';
+import './styles/main.css';
+import './styles/util.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
